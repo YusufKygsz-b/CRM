89bb1dcdb6effb570520de79b00c56089db098e9
Yusuf B <yusufkygsz.b@gmail.com>
2024-08-06 15:18:32 +0300

    Stable Version 1.2

--- b_crm/website/consumers.py
+++ b_crm/website/consumers.py
@@ -1,26 +1,34 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+import logging
+
+logger = logging.getLogger(__name__)
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        logger.info('WebSocket connection established')
         await self.channel_layer.group_add(
-            "notifications",
+            'notifications',
             self.channel_name
         )
         await self.accept()
 
     async def disconnect(self, close_code):
+        logger.info('WebSocket connection closed')
         await self.channel_layer.group_discard(
-            "notifications",
+            'notifications',
             self.channel_name
         )
 
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        verb = text_data_json['verb']
-
+        data = json.loads(text_data)
+        verb = data.get('verb', None)
+        logger.info(f'Received verb: {verb}')
+        if verb is None:
+            logger.error('Verb key is missing in received data')
+            return
         await self.channel_layer.group_send(
-            "notifications",
+            'notifications',
             {
                 'type': 'send_notification',
                 'verb': verb
@@ -28,8 +36,11 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         )
 
     async def send_notification(self, event):
-        verb = event['verb']
-
+        verb = event.get('verb', None)
+        logger.info(f'Sending verb: {verb}')
+        if verb is None:
+            logger.error('Verb key is missing in event data')
+            return
         await self.send(text_data=json.dumps({
             'verb': verb
         }))
--- b_crm/website/views.py
+++ b_crm/website/views.py
@@ -1,3 +1,141 @@
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import authenticate, login, logout
+# from .forms import SignUpForm, AddRecordForm
+# from .models import Record
+# from notifications.signals import notify
+# from notifications.models import Notification
+# from django.contrib.auth.models import User
+# from django.http import JsonResponse
+
+# from channels.layers import get_channel_layer
+# from asgiref.sync import async_to_sync
+
+
+# def my_view(request):
+#     # Bildirim oluşturma işlemi
+#     user = request.user
+#     notify.send(user, recipient=user, verb='Yeni bir bildirim!')
+
+#     # Kanal katmanı
+#     channel_layer = get_channel_layer()
+#     async_to_sync(channel_layer.group_send)(
+#         f"user_{user.id}",
+#         {
+#             'type': 'notification_message',
+#             'verb': 'Yeni bir bildirim!'
+#         }
+#     )
+
+# from channels.layers import get_channel_layer
+# from asgiref.sync import async_to_sync
+
+
+# def home(request):
+#     records = Record.objects.all()
+#     notifications = []
+
+#     if request.user.is_authenticated:
+#         notifications = Notification.objects.filter(recipient=request.user, deleted=False).order_by('-timestamp')
+
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             print(f"Authenticated user: {user}")
+#             notify.send(user, recipient=user, verb='Giriş yaptınız!')
+#             return redirect('home')
+#         else:
+#             if request.user.is_authenticated:
+#                 notify.send(request.user, recipient=request.user, verb='Girişte bir hata oluştu, Lütfen Tekrar Deneyiniz...')
+#             return redirect('home')
+
+#     return render(request, 'home.html', {'records': records, 'notifications': notifications})
+
+
+# def logout_user(request):
+#     if request.user.is_authenticated:
+#         user = request.user
+#         logout(request)
+#         notify.send(user, recipient=user, verb='Çıkış yaptınız...')
+#         return redirect('home')
+#     else:
+#         return redirect('home')
+    
+# def register_user(request):
+#     if request.method == "POST":
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password1']
+#             user = authenticate(username=username, password=password)
+#             login(request, user)
+#             notify.send(user, recipient=user, verb='Başarılı bir şekilde kayıt oldunuz, Hoşgeldiniz')
+#             return redirect('home')
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'register.html', {'form': form})
+
+# def customer_record(request, pk):
+#     if request.user.is_authenticated:
+#         customer_record = Record.objects.get(id=pk)
+#         return render(request, 'record.html', {'customer_record': customer_record})
+#     else:
+#         notify.send(None, recipient=request.user, verb='Bu Sayfayı Görüntülemek İçin Giriş Yapmış Olmalısınız...')
+#         return redirect('home')
+
+# def delete_record(request, pk):
+#     if request.user.is_authenticated:
+#         delete_it = Record.objects.get(id=pk)
+#         delete_it.delete()
+#         notify.send(request.user, recipient=request.user, verb='Kayıt Başarıyla Silindi...')
+#         return redirect('home')
+#     else:
+#         notify.send(None, recipient=request.user, verb='Bunu Yapmak İçin Giriş Yapmış Olmalısınız...')
+#         return redirect('home')
+
+# def add_record(request):
+#     form = AddRecordForm(request.POST or None)
+#     if request.user.is_authenticated:
+#         if request.method == "POST":
+#             if form.is_valid():
+#                 form.save()
+#                 notify.send(request.user, recipient=request.user, verb='Kayıt Eklendi...')
+#                 return redirect('home')
+#         return render(request, 'add_record.html', {'form': form})
+#     else:
+#         notify.send(None, recipient=request.user, verb='Giriş Yapmış Olmalısınız...')
+#         return redirect('home')
+
+# def update_record(request, pk):
+#     if request.user.is_authenticated:
+#         current_record = Record.objects.get(id=pk)
+#         form = AddRecordForm(request.POST or None, instance=current_record)
+#         if form.is_valid():
+#             form.save()
+#             notify.send(request.user, recipient=request.user, verb='Record Has Been Updated!')
+#             return redirect('home')
+#         return render(request, 'update_record.html', {'form': form})
+#     else:
+#         notify.send(None, recipient=request.user, verb='You Must Be Logged In...')
+#         return redirect('home')
+
+# def mark_as_deleted(request, id):
+#     if request.method == 'POST':
+#         try:
+#             notification = Notification.objects.get(id=id)
+#             notification.deleted = True
+#             notification.save()
+#             return JsonResponse({'status': 'success'})
+#         except Notification.DoesNotExist:
+#             return JsonResponse({'status': 'error', 'message': 'Notification not found'}, status=404)
+#     else:
+#         return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
+
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import SignUpForm, AddRecordForm
@@ -6,29 +144,18 @@ from notifications.signals import notify
 from notifications.models import Notification
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
-
-def my_view(request):
-    # Bildirim oluşturma işlemi
-    user = request.user
-    notify.send(user, recipient=user, verb='Yeni bir bildirim!')
-
-    # Kanal katmanı
+def send_notification_to_all_users(verb):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
-        f"user_{user.id}",
+        "notifications",  # Genel kanal adı
         {
-            'type': 'notification_message',
-            'verb': 'Yeni bir bildirim!'
+            'type': 'send_notification',
+            'verb': verb
         }
     )
-
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
-
 
 def home(request):
     records = Record.objects.all()
@@ -44,8 +171,8 @@ def home(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            print(f"Authenticated user: {user}")
             notify.send(user, recipient=user, verb='Giriş yaptınız!')
+            send_notification_to_all_users('Yeni giriş yapıldı!')
             return redirect('home')
         else:
             if request.user.is_authenticated:
@@ -54,16 +181,16 @@ def home(request):
 
     return render(request, 'home.html', {'records': records, 'notifications': notifications})
 
-
 def logout_user(request):
     if request.user.is_authenticated:
         user = request.user
         logout(request)
         notify.send(user, recipient=user, verb='Çıkış yaptınız...')
+        send_notification_to_all_users('Bir kullanıcı çıkış yaptı!')
         return redirect('home')
     else:
         return redirect('home')
-    
+
 def register_user(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
@@ -92,6 +219,7 @@ def delete_record(request, pk):
         delete_it = Record.objects.get(id=pk)
         delete_it.delete()
         notify.send(request.user, recipient=request.user, verb='Kayıt Başarıyla Silindi...')
+        send_notification_to_all_users('Bir kayıt silindi!')
         return redirect('home')
     else:
         notify.send(None, recipient=request.user, verb='Bunu Yapmak İçin Giriş Yapmış Olmalısınız...')
@@ -103,7 +231,9 @@ def add_record(request):
         if request.method == "POST":
             if form.is_valid():
                 form.save()
-                notify.send(request.user, recipient=request.user, verb='Kayıt Eklendi...')
+                message = 'Kayıt Eklendi...'
+                notify.send(request.user, recipient=request.user, verb=message)
+                send_notification_to_all_users(message)  # Tüm kullanıcılara bildirim gönder
                 return redirect('home')
         return render(request, 'add_record.html', {'form': form})
     else:
@@ -116,11 +246,13 @@ def update_record(request, pk):
         form = AddRecordForm(request.POST or None, instance=current_record)
         if form.is_valid():
             form.save()
-            notify.send(request.user, recipient=request.user, verb='Record Has Been Updated!')
+            message = 'Kayıt Güncellendi!'
+            notify.send(request.user, recipient=request.user, verb=message)
+            send_notification_to_all_users(message)  # Tüm kullanıcılara bildirim gönder
             return redirect('home')
         return render(request, 'update_record.html', {'form': form})
     else:
-        notify.send(None, recipient=request.user, verb='You Must Be Logged In...')
+        notify.send(None, recipient=request.user, verb='Giriş Yapmış Olmalısınız...')
         return redirect('home')
 
 def mark_as_deleted(request, id):
