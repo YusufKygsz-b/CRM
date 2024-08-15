a3d4e7d21ecddc542b37aef6c4a3e6ab9f949911
Yusuf B <yusufkygsz.b@gmail.com>
2024-08-15 14:31:21 +0300

    web-socket & notification setting is done

--- b_crm/b_crm/urls.py
+++ b_crm/b_crm/urls.py
@@ -2,21 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
-from rest_framework.routers import DefaultRouter
-from website.views import SupplierViewSet
 
-# Router'ı oluşturun ve API view'ını ekleyin
-router = DefaultRouter()
-router.register(r'suppliers', SupplierViewSet)
-
-# URL desenlerini tanımlayın
 urlpatterns = [
     path(_('admin/'), include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('crm_login/', admin.site.urls),
+    path('', include('website.urls')),
 ]
 
-# Uluslararasılaştırma (i18n) desenlerini ekleyin
 urlpatterns += i18n_patterns(
-    path('', include('website.urls')),  # Diğer uygulama URL'lerini buraya ekleyin
-    path('api/', include(router.urls)),  # API URL'lerini buraya ekleyin
+    path('', include('website.urls')),  # Dil URL deseni için 'website' uygulamasını ekledim
 )
